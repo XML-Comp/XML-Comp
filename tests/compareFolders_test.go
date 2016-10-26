d4043fa204636e7f0f148160667a3ec23af846d1
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-10-26 23:22:20 +0530

    Few changes

--- tests/compareFolders_test.go
+++ tests/compareFolders_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"XML-Comp/comparer"
+	"os"
 	"testing"
 )
 
@@ -18,5 +19,26 @@ func TestCompareFolder(t *testing.T) {
 	if instance.CompareContainingFoldersAndFiles() == nil {
 		//If error nil, then the validation was somehow bypassed
 		t.Error("Oops! Error in Path validation")
+	}
+
+	//Testing for actual folders
+	//the code will create 2 directories with files
+	tmpDir := os.TempDir()
+	pathA := tmpDir + "/_Dir1/subDir/superSub"
+	err := os.MkdirAll(pathA, 0777)
+	if err != nil {
+		t.Fatalf("MkdirAll %q: %s", pathA, err)
+	}
+
+	pathB := tmpDir + "/_Dir2/subDir"
+	err = os.MkdirAll(pathB, 0777)
+	if err != nil {
+		t.Fatalf("MkdirAll %q: %s", pathB, err)
+	}
+	//Temporary directories created
+
+	instance = comparer.Data{PathA: pathA, PathB: pathB}
+	if instance.CompareContainingFoldersAndFiles() != nil {
+		t.Error("Oops! seems like a logical error")
 	}
 }
