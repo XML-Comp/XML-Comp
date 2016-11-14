99ba40ab2ec50451e2f0c776655b78af8acc4881
Arthur Silva <arthur.001.002@gmail.com>
2016-11-14 14:09:08 -0200

    main: version flag added, fix #15

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 	"github.com/ArxdSilva/XML-Comp/comparer"
 )
 
+const ver = "v0.0.4"
+
 func main() {
 	var (
 		original    = flag.String("original", "", "Full path directory of your RimWorld English folder (required)")
 		translation = flag.String("translation", "", "Full path directory of your RimWorld Translation folder (required)")
+		version     = flag.Bool("version", false, "Prints current version")
 	)
 	flag.Parse()
 	args := os.Args
@@ -19,6 +22,10 @@ func main() {
 	if len(args) < 2 || args[1] == "-h" {
 		flag.Usage()
 		os.Exit(1)
+	}
+	if *version {
+		fmt.Println(ver)
+		os.Exit(0)
 	}
 	// If either original or translation not provided exit
 	if len(*original) == 0 || len(*translation) == 0 {
