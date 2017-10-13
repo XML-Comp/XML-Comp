876133c2a7b0072d622f597e65a184da18d272f5
Anthony Alves <cvballa3g0@gmail.com>
2017-10-12 22:25:44 -0400

    main: check for default paths of the english translation folder.
    This function checks if the user is on Windows/Mac & which architecture
    to find the Steam folder in. If the English Translation folder is not
    found, the user must supply the -original argument.

--- main.go
+++ main.go
@@ -8,13 +8,15 @@ import (
 	"path/filepath"
 
 	"github.com/XML-Comp/XML-Comp/comparer"
+	"os/user"
+	"runtime"
 )
 
 const ver = "v0.4"
 
 func main() {
 	var (
-		original            = flag.String("original", "", "Full path directory of your RimWorld English folder (required)")
+		original            = flag.String("original", getOriginalDir(), "Full path directory of your RimWorld English folder (required)")
 		translation         = flag.String("translation", "", "Full path directory of your RimWorld Translation folder (required)")
 		docType             = flag.String("doc", "xml", "Type of the Doc that you want to compare (not required)")
 		multipleMsg         = "Considers the translation flag as a collection of translations, enabling 1:N comparison"
@@ -66,4 +68,38 @@ func main() {
 	fmt.Println("Docs comparisons are DONE!")
 	fmt.Printf("Documents scanned: %v | Lines scanned: %v | Translations needed: %v\n", docs, lines, inNeed)
 	os.Exit(0)
+}
+
+// getOriginalDir gets the default Steam installation directory.
+// Returns empty string if directory is not found.
+func getOriginalDir() string {
+	rimWorldDir := ""
+
+	// https://support.steampowered.com/kb_article.php?ref=7710-tdlc-0426
+	if runtime.GOOS == "windows" && runtime.GOARCH == "386" {
+		rimWorldDir = `C:\Program Files\Steam\steamapps\common\RimWorld`
+	} else if runtime.GOOS == "windows" && runtime.GOARCH == "amd64" {
+		rimWorldDir = `C:\Program Files (x86)\Steam\steamapps\common\RimWorld`
+	} else if runtime.GOOS == "darwin" {
+		// https://gaming.stackexchange.com/a/219537
+		currentUser, err := user.Current()
+		if err != nil {
+			return ""
+		}
+
+		// safe to use *nix path sep since we are in the darwin runtime
+		rimWorldDir = filepath.Join(currentUser.HomeDir,
+			`/Library/Application Support/Steam/steamapps/common/RimWorld/RimWorldMac.app`)
+	}
+
+	// we're in all runtimes here, use os dependent separator
+	englishTrans := filepath.Join(rimWorldDir, "Mods", "Core", "Languages", "English")
+
+	// do one final check to make sure the English dir exists
+	_, err := os.Stat(englishTrans)
+	if err != nil {
+		englishTrans = ""
+	}
+
+	return englishTrans
 }
